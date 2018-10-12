7eebb9c53bdeb8096b4c58cc068ec847d02f8ad2
unknown <amandaaw23@gmail.com>
2018-10-12 08:48:36 -0400

    added router

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PollContainer from '../containers/PollContainer';
+import {BrowserRouter} from 'react-router-dom';
+import App from '../containers/App.js';
+import AboutUs from '../components/AboutUs.js';
 
-ReactDOM.render(
-    <PollContainer />,
-    document.getElementById('app')
+// ReactDOM.render(
+//     <PollContainer />,
+//     document.getElementById('app')
+// );
+
+ReactDOM.render((
+	<BrowserRouter>
+		<App/>
+	</BrowserRouter>),
+	document.getElementById('app')
 );
